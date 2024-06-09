00bac7d4d6e4a8201da17c7921517d527b3bb810
sindy sira <sindysira9@gmail.com>
2024-06-09 22:10:18 +0000

    first commit

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
- /*document.write("a")
- document.write("a")
- document.write("a")
- document.write("a")
- document.write("a")
- */
-// let i = 0;
-
-// while(i < 5)
-//     {
-//         document.write("a");
-//         i++;
-//     }
-
-// let i = 10;
-// do{
-//     document.write("AAA")
-//     i++;
-// }
-// while(i < 10);
-
-for(let i = 0; i < 50; ++i )
+document.write("hello ");
+document.write("World !");
+/*let i = 0;
+while(i < 10)
     {
-        document.write("a")
-
-        if(i == 5)
-            continue;
-        
-        document.write("b ");
+        document.write("a");
+        i++;
     }
+*/
+/*let i = 10;
+do
+{
+    document.write("a");
+    i++;
+}
+while(i < 10);
+*/
+/*
+for(let i = 0 ; i < 5 ; ++i)
+{
+    document.write("a");
+}
+*/
+
+for(let i = 0 ; i < 5 ; ++i)
+    {
+        document.write("a");
+        document.write("b ");
+    } 
