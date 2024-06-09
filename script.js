3ab2c1ecfdbe2f3e88ce7521ae7832b770ba0915
sindy sira <sindysira9@gmail.com>
2024-06-09 00:09:32 +0000

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-document.write("Hello Word !")
-console.error("probleme")
+// var age = 14;
+
+// age = 14;
+
+// document.write(age);
+
+var age = prompt("Quel est ton age ? ");
+age = parseInt(age);
+document. write(typeof age);
+
+const pi =3.14;
+pi =2;
+document.write(pi)
