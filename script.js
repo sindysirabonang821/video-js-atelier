5f81d73903d57b088decb8f2b2da55a803f4df00
sindy sira <sindysira9@gmail.com>
2024-06-09 17:07:34 +0000

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-//  document.write((5 / 2))
-let N = 5;
-// N = N + 1;
-// document.write(++N);
-/* =, +=; -=;/= %=
-++;--
-+; -; *; /; %
-==;===,!=,!==
-*/
+// let name = "yuna"
+// if(name === "yuna")
+//     {
+//         document.write("OUI")
+//         let number = 6;
+//         if(number === 6);
+//         document.write("oui")
+//     }
+//     else
+//     {
+//         document.write("NON")
+//     }
 
-document.write(8 < 12)
+// let number = 6;
+// if(number < 0);
+// {
+//     document.write("< 0");
+//   else  if( "number > 10");
+//    document.write("> 10");
+// }
+// else
+// document.write(number);
 
+let number = 5;
+switch(number)
+{
+    case 0:
+    case 1:
+    case 2:
+     case 3:
+    case 4:
+    document.write("number < 5")
+    break
+    case 5:
+    document.write("number = 5") 
+    break   
+    default: alert("pas de valeure reconnue ....")
+    break
+}
+    
